9f82a60eeffc0ff97e26bdfeec0ec56529720b99
206-014\206-014 <hanzen927@gmail.com>
2016-09-23 15:42:48 +0900

    zz

--- SimpleMVC/src/main/java/net/gondor/index/biz/NewsBizImpl.java
+++ SimpleMVC/src/main/java/net/gondor/index/biz/NewsBizImpl.java
@@ -1,5 +1,41 @@
 package net.gondor.index.biz;
 
+import java.util.List;
+
+import net.gondor.index.dao.NewsDao;
+import net.gondor.index.dao.NewsDaoImpl;
+import net.gondor.index.vo.NewsVO;
+
 public class NewsBizImpl implements NewsBiz{
+
+	private NewsDao dao;
+	
+	public NewsBizImpl(){
+		dao =new NewsDaoImpl();
+	}
+	
+	@Override
+	public List<NewsVO> getAllNewsesOf(int categoryId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addNewNews(NewsVO news) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public NewsVO getNewAt(int newsId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteNewsAt(int newsId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
